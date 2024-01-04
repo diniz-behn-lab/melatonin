cdd4a2dfbe432674f1df5d97663f8cfcf3047656
Shelby Stowe (Student) <sstowe@mines.edu>
2024-01-04 15:09:52 -0700

    Worked on creating the model class

--- Hannay-Breslow.py
+++ Hannay-Breslow.py
@@ -32,53 +32,204 @@ from HCRSimPY.models import SinglePopModel
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
+from scipy import integrate
 
 # ------- Create Model Class -------------------
 
+class IntegratedModel(SinglePopModel):
+    """Driver of ForwardModel simulation"""
+
+    def __init__(self, LightFun):
+        super().__init__(LightFun)
+        self.set_params()
+
+    def set_params(self):
+        ## Breslow Model
+        self.beta_IP = 7.83e-4*60*60
+        self.beta_CP = 3.35e-4*60*60
+        self.beta_AP = 1.62-4*60*60
+
+        self.a = 600
+        self.r = 15.36
+
+        self.psi_on = 6.113
+        self.psi_off = 4.352
+
+        self.M_max = 0.019513
+        self.H_sat = 861
+        self.sigma_M = 50
+        self.m = 7
+
+        ## Hannay Model
+        self.D = 1
+        self.gamma = 0.024
+        self.K = 0.06358 
+        self.beta = -0.09318
+        self.omega_0 = 0.263524
+
+        self.A_1 = 0.3855 
+        self.beta_L1 = -0.0026
+        self.A_2 = 0.1977 
+        self.beta_L2 = -0.957756
+        self.sigma = 0.0400692
+
+        self.delta = 0.0075
+        self.alpha_0 = 0.05
+        self.p = 1.5
+        self.I_0 = 9325
+        self.G = 33.75
+
+        self.B_1 = 1
+        self.theta_M1 = 1
+        self.B_2 = 1
+        self.theta_M2 = 1
+        self.epsilon = 1
+        
+
+        """"Not sure about this stuff"""
+        """
+        r2 = self.beta_cp
+        r3 = self.beta_ap
+        max_ratio = (r3/(r2-r3))*(np.power(r2/r3,-r3/(r2-r3))-np.power(r2/r3,-r2/(r2-r3)))
+        mid_dose = (0.1+0.3)/2
+        dose_ratio = 200/mid_dose
+        self.aborption_conversion = dose_ratio/max_ratio
+        ## EFFICIENCY of 0.004 injestd -> blood plasma
+        """
 
 
-# Parameter values
+    def m_process(self,u):
+        H2 = max(u[4],0)
+        H2_conc = H2*861/200 # Convert from pg/L to pmol/L
+        try:
+            if H2_conc > 861*10:
+                output = self.M_max
+            else:
+                output = self.M_max/(1 + np.exp((self.H_sat - H2_conc)/self.sigma_m))
+        except:
+            print("Slow the heck down there")
+            output = self.M_max/(1 + np.exp((self.H_sat - H2_conc)/self.sigma_m))
+        return output
 
-## Breslow Model
-beta_IP = 7.83e-4*60*60
-beta_CP = 3.35e-4*60*60
-beta_AP = 1.62-4*60*60
+    def circ_response(self,phi):
+        dlmo_phase = 5*np.pi/12
+        phi = np.mod(phi - dlmo_phase,2*np.pi)
+        a = 4*60
+        phi_off = 4.3 
+        phi_on = 6.1
+        delta = 600/3600
+        r = 15.36/3600
 
-a = 600
-r = 15.36
+        if phi > phi_off and phi <= phi_on:
+            return a * (1 - np.exp(-delta*np.mod(phi_on - phi,2*np.pi))) / (1 - np.exp(-delta*np.mod(phi_on - phi_off,2*np.pi)))
+        else:
+            return a*np.exp(-r*np.mod(phi_on - phi_off,2*np.pi))
 
-psi_on = 6.113
-psi_off = 4.352
+    def mel_derv(self,t,u,bhat):
+        H1 = max(u[3],0)
+        H2 = max(u[4],0)
+        H3 = max(u[5],0)
 
-M_max = 0.019513
-H_sat = 861
-sigma_M = 50
-m = 7
+        m = 7*60 # maybe m = 7*3600?
+        beta_ip = 7.83e-4*3600
+        beta_cp = 3.35e-4*3600
+        beta_ap = 1.62e-4*3600
+        
+        tmp = 1 - m*bhat # This m might need to be altered
+        S = not(H1 < 0.001 and tmp < 0)
 
-## Hannay Model
-D = 1
-gamma = 0.024
-K = 0.06358 
-beta = -0.09318
-omega_0 = 0.263524
+        dH1dt = -beta_ip*H1 + self.circ_response(u[1])*tmp*S
+        dH2dt = beta_ip*H1 - beta_cp*H2 + beta_ap*H3
+        dH3dt = -beta_ap*H3
 
-A_1 = 0.3855 
-beta_L1 = -0.0026
-A_2 = 0.1977 
-beta_L2 = -0.957756
-sigma = 0.0400692
+        dHdt = np.array([dH1dt,dH2dt,dH3dt])
+        dHdt = np.sign(dHdt)*np.minimum(60*200*np.ones(3),np.abs(dHdt))
+        return dHdt
 
-delta = 0.0075
-alpha_0 = 0.05
-p = 1.5
-I_0 = 9325
-G = 33.75
+    def derv(self,t,y):
+        """
+        This defines the ode system for the single population model.
+        derv(self,t,y)
+        returns dydt numpy array.
+        """
+        R=y[0]
+        Psi=y[1]
+        n=y[2]
+        H1 = y[3]
+        H2 = y[4]
+        H3 = y[5]
 
-B_1 = 1
-theta_M1 = 1
-B_2 = 1
-theta_M2 = 1
-epsilon = 1
+        # Light interaction with pacemaker
+        Bhat=self.G*(1.0-n)*self.alpha0(t)
+        LightAmp=self.A1*0.5*Bhat*(1.0-pow(R,4.0))*np.cos(Psi+self.BetaL1)+self.A2*0.5*Bhat*R*(1.0-pow(R,8.0))*np.cos(2.0*Psi+self.BetaL2)
+        LightPhase=self.sigma*Bhat-self.A1*Bhat*0.5*(pow(R,3.0)+1.0/R)*np.sin(Psi+self.BetaL1)-self.A2*Bhat*0.5*(1.0+pow(R,8.0))*np.sin(2.0*Psi+self.BetaL2)
+
+        dydt=np.zeros(6)
+
+        # Melatonin variables
+        dHdt = self.mel_derv(t,y,Bhat)
+        dydt[3:] = dHdt
+        
+        # Melatonin interaction with pacemaker
+        Mhat = self.m_process(y)
+        MelAmp = self.B1*0.5*Mhat*(1.0-pow(R,4.0))*np.cos(Psi+self.theta1)+self.B2*0.5*Mhat*R*(1.0-pow(R,8.0))*np.cos(2.0*Psi+self.theta2)
+        MelPhase = self.epsilon*Mhat-self.B1*Mhat*0.5*(pow(R,3.0)+1.0/R)*np.sin(Psi+self.theta1)-self.B2*Mhat*0.5*(1.0+pow(R,8.0))*np.sin(2.0*Psi+self.theta2)
+
+        dydt[0]=-1.0*self.gamma*R+self.K*np.cos(self.Beta1)/2.0*R*(1.0-pow(R,4.0))+LightAmp+MelAmp
+        dydt[1]=self.w0+self.K/2.0*np.sin(self.Beta1)*(1+pow(R,4.0))+LightPhase+MelPhase
+        dydt[2]=60.0*(self.alpha0(t)*(1.0-n)-self.delta*n)
+
+
+        return(dydt)
+
+
+
+    def integrateModel(self, tend, tstart=0.0, initial=[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],melatonin_timing = None,melatonin_dosage=None):
+        """ Integrate the model forward in time.
+        integrateModel(tend, initial=[1.0,0.0, 0.0])
+        tend: float giving the final time to integrate to.
+        initial: initial dynamical state
+        The parameters are tend= the end time to stop the simulation and initial=[R, Psi, n]
+        Writes the integration results into the scipy array self.results.
+        Returns the circadian phase (in hours) at the ending time for the system.
+        """
+        dt = 0.1
+        self.ts = np.arange(tstart,tend,dt)
+        initial[1] = np.mod(initial[1], 2*sp.pi) #start the initial phase between 0 and 2pi
+
+        # Sanitize input: sometimes np.arange witll output values outside of the range of tstart -> tend
+        self.ts = self.ts[self.ts <= tend]
+        self.ts = self.ts[self.ts >= tstart]
+        
+        if melatonin_timing is None:
+            r = sp.integrate.solve_ivp(self.derv,(tstart,tend), initial, t_eval=self.ts, method='Radau')
+            self.results = np.transpose(r.y)
+        else:
+            t_start = self.ts[0]
+            all_arrays = []
+            for t_end in melatonin_timing:
+                local_ts = self.ts[np.logical_and(self.ts >= t_start, self.ts <= t_end)]
+                r = sp.integrate.solve_ivp(self.derv,(t_start,t_end+0.1), initial, t_eval = local_ts, method="Radau")
+                all_arrays.append(r.y[:,:-1])
+                initial = r.y[:,-1]
+                initial[5] = initial[5] + melatonin_dosage*self.aborption_conversion
+
+
+                t_start = t_end
+            t_end = tend
+            local_ts = self.ts[np.logical_and(self.ts >= t_start, self.ts <= t_end)]
+            r = sp.integrate.solve_ivp(self.derv,(t_start,t_end+0.1), initial, t_eval = local_ts, method="Radau") # might be a way to constrain parameter values, help ensure there are no negative concentrations for example
+            all_arrays.append(r.y)
+            results = all_arrays[0]
+            for i in np.arange(1,len(all_arrays)):
+                results = np.hstack((results,all_arrays[i]))
+
+            self.results = np.transpose(results)
+
+        
+        
+        return
+
 
 
 
