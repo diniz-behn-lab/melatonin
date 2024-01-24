550f5fd3e82519fb07cd427322f46e588446b0c6
Shelby Stowe (Student) <sstowe@mines.edu>
2024-01-24 10:03:23 -0700

    Adding comments

--- Hannay-Breslow.py
+++ Hannay-Breslow.py
@@ -243,8 +243,11 @@ class HannayBreslowModel(object):
 model = HannayBreslowModel() # defining model as a new object built with the HannayBreslowModel class 
 model.integrateModel(24*10) # use the integrateModel method with the object model
 IC = model.results[-1,:] # get initial conditions from entrained model
+
+#Uncomment this one to run it without exogenous melatonin
 #model.integrateModel(24*1,tstart=0.0,initial=IC, melatonin_timing=None, melatonin_dosage=None) # run the model from entrained ICs
 
+#Uncomment this one to run it with exogenous melatonin 
 model.integrateModel(24*1,tstart=0.0,initial=IC, melatonin_timing=8.0+24*np.arange(5), melatonin_dosage=0.0)
 
 
