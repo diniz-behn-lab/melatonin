acd1c254c74f95ab11a45c19338024bb2e89b976
Shelby Stowe (Student) <sstowe@mines.edu>
2024-02-03 18:23:32 -0700

    Update Hannay-Breslow.py

--- Hannay-Breslow.py
+++ Hannay-Breslow.py
@@ -241,7 +241,7 @@ IC = model.results[-1,:] # get initial conditions from entrained model
 #model.integrateModel(24*5,tstart=0.0,initial=IC, melatonin_timing=None, melatonin_dosage=None) # run the model from entrained ICs
 
 #Uncomment this one to run it with exogenous melatonin 
-model.integrateModel(24*5,tstart=0.0,initial=IC, melatonin_timing=20.0, melatonin_dosage=10)
+model.integrateModel(24*1,tstart=0.0,initial=IC, melatonin_timing=20.0, melatonin_dosage=40)
 
 
 
