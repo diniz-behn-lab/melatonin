57461dd4880917e915ced5bd365a84eb0b06dd2e
Shelby Stowe (Student) <sstowe@mines.edu>
2024-02-06 14:59:05 -0700

    Adding another schedule

--- Light_schedule.py
+++ Light_schedule.py
@@ -10,15 +10,18 @@ Code to generate plot of light level
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+#------------- Standard Light Schedule (16:8) --------------
+
 j = 2 # Number of days to be plotted
 
 n = j*24
 
-light_level = list()
+standard_lightschedule = list()
 
 for t in range(0, n):
-    full_light = 10#1000
-    dim_light = 10#300 # reduced light
+    full_light = 1000
+    dim_light = 300 # reduced light
     wake_time = 7
     sleep_time = 23
     sun_up = 8
@@ -27,8 +30,36 @@ for t in range(0, n):
     is_awake = np.mod(t - wake_time,24) <= np.mod(sleep_time - wake_time,24)
     sun_is_up = np.mod(t - sun_up,24) <= np.mod(sun_down - sun_up,24)
 
-    light_level.append(is_awake*(full_light*sun_is_up + dim_light*(1 - sun_is_up)))
+    standard_lightschedule.append(is_awake*(full_light*sun_is_up + dim_light*(1 - sun_is_up)))
 
-    #return light_level
         
-plt.plot(range(0,n),light_level)
+plt.plot(range(0,n),standard_lightschedule)
+plt.title("Standard Light:Dark Schedule (16:8)")
+plt.show()
+
+
+#------------------- Burgess 2008 Ultradian Light Schedule ------------
+
+j = 5 # Number of days to be plotted
+
+n = j*24
+
+standard_lightschedule = list()
+
+for t in range(0, n):
+    full_light = 1000
+    dim_light = 300 # reduced light
+    wake_time = 7
+    sleep_time = 23
+    sun_up = 8
+    sun_down = 19
+
+    is_awake = np.mod(t - wake_time,24) <= np.mod(sleep_time - wake_time,24)
+    sun_is_up = np.mod(t - sun_up,24) <= np.mod(sun_down - sun_up,24)
+
+    standard_lightschedule.append(is_awake*(full_light*sun_is_up + dim_light*(1 - sun_is_up)))
+
+        
+plt.plot(range(0,n),standard_lightschedule)
+plt.title("Standard Light:Dark Schedule (16:8)")
+plt.show()
