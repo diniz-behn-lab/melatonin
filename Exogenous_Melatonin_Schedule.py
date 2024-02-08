cb0eab74e26b78e86bce5fba5609b2bbcf5f3c10
Shelby Stowe (Student) <sstowe@mines.edu>
2024-02-08 13:46:33 -0700

    Update Exogenous_Melatonin_Schedule.py

--- Exogenous_Melatonin_Schedule.py
+++ Exogenous_Melatonin_Schedule.py
@@ -79,7 +79,7 @@ j = 3 # Number of days to be plotted
 n = j*24
 
 T = np.linspace(0,n,720)
-T_round = np.round(T)
+T_round = np.round(T,decimals=1)
 
 mel_timing = 10.0+24*np.arange(j)
 
@@ -88,15 +88,18 @@ Times = np.intersect1d(mel_timing, T_round, return_indices=True)[2]
 ex_melatonin = signal.unit_impulse(720,Times)
 
 plt.plot(T,ex_melatonin)
+plt.title("Unit Pulse")
 plt.show()
 
 
 #---------------------------------
 
 def smooth_pulse(time, start, stop, cycle=1, steep=1000):
-    xi = 3/4 - (stop - start) / (2 * cycle) * (cycle / (stop - start))
-    return 1 / (1 + np.exp(steep * (np.sin(2 * np.pi * ((time - start) / cycle + xi)) - np.sin(2 * np.pi * xi))))
-    #return (cycle/(stop - start)) / (1 + np.exp(steep * (np.sin(2 * np.pi * ((time - start) / cycle + xi)) - np.sin(2 * np.pi * xi))))
+    #xi = 3/4 - (stop - start) / (2 * cycle) * (cycle / (stop - start))
+    #return 1 / (1 + np.exp(steep * (np.sin(2 * np.pi * ((time - start) / cycle + xi)) - np.sin(2 * np.pi * xi))))
+    
+    xi = 3/4 - (stop - start) / (2 * cycle)
+    return (cycle/(stop - start)) / (1 + np.exp(steep * (np.sin(2 * np.pi * ((time - start) / cycle + xi)) - np.sin(2 * np.pi * xi))))
 
 x = np.arange(0, 5, 0.001)
 y_smooth = smooth_pulse(x, start=0.2, stop=0.3, cycle=0.5)
@@ -107,6 +110,29 @@ plt.xlabel('Time')
 plt.ylabel('f(t)')
 plt.title('Smooth Differentiable Pulse Function')
 plt.xlim(0, 2)
+plt.ylim(0, 1)
+plt.grid(True)
+plt.legend()
+plt.show()
+
+#--------------------------------------
+
+def smooth_pulse(time, start, stop, cycle=1, steep=1000):
+    #xi = 3/4 - (stop - start) / (2 * cycle) * (cycle / (stop - start))
+    #return 1 / (1 + np.exp(steep * (np.sin(2 * np.pi * ((time - start) / cycle + xi)) - np.sin(2 * np.pi * xi))))
+    
+    xi = 3/4 - (stop - start) / (2 * cycle)
+    return (cycle/(stop - start)) / (1 + np.exp(steep * (np.sin(2 * np.pi * ((time - start) / cycle + xi)) - np.sin(2 * np.pi * xi))))
+
+x = np.arange(0, 48, 0.001)
+y_smooth = smooth_pulse(x, start=12.0, stop=12.1, cycle=24)
+
+
+plt.plot(x, y_smooth, label='Smooth Pulse')
+plt.xlabel('Time')
+plt.ylabel('f(t)')
+plt.title('Smooth Differentiable Pulse Function')
+plt.xlim(0, 24)
 plt.ylim(0, 1)
 plt.grid(True)
 plt.legend()
