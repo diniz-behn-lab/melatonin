b81413ba27d929fec39494fcd1801ffbde9398a3
Shelby Stowe (Student) <sstowe@mines.edu>
2024-02-21 17:05:01 -0700

    Trying new psi_on and psi_off values
    Not working

--- Hannay-Breslow_Burgess2008.py
+++ Hannay-Breslow_Burgess2008.py
@@ -34,12 +34,12 @@ class HannayBreslowModel(object):
         self.beta_CP = 3.35e-4*60*60 #converting 1/sec to 1/hr
         self.beta_AP = 1.62e-4*60*60 #converting 1/sec to 1/hr
 
-        self.a = 6*60 #1.0442e-3, the tiny value is from Breslow 
+        self.a = 6.25*60 #6*60 #1.0442e-3, the tiny value is from Breslow 
         self.delta_M = 600/3600 # converting secs to hrs
         self.r = 15.36/3600 # converting secs to hrs
 
-        self.psi_on = 6.113
-        self.psi_off = 4.352
+        self.psi_on = (23*np.pi/12) #1.0472 #2.44346095 #2.61799 #1.0472 #6.113 CHANGED 
+        self.psi_off = (4*np.pi/12) #3.92699 #3.57792497 #3.40339204 #4.352 CHANGED
 
         self.M_max = 0.019513
         self.H_sat = 861
