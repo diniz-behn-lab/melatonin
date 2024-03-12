ff13bbad8795380b042e378dc9b8159f05e6c60c
Shelby Stowe (Student) <sstowe@mines.edu>
2024-03-11 20:53:05 -0600

    Create InstantaneousPRCs.py

---
+++ March 2024/InstantaneousPRCs.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 20:46:48 2024
+
+@author: sstowe
+
+Plotting the instantaneous phase response curves to light and melatonin 
+
+"""
+
+
+#-------- Set Up -------------------------------
+
+# Imports 
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+psi = np.linspace(0,2*np.pi,100)
+
