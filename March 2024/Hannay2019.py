007f23fefd96ce2beebf1f64526027feea5d847b
Shelby Stowe (Student) <sstowe@mines.edu>
2024-03-14 16:19:34 -0600

    Create Hannay2019.py

---
+++ March 2024/Hannay2019.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 16:15:19 2024
+
+@author: sstowe
+
+Code to run original Hannay model
+
+"""
+
+
+#-------- Set Up -------------------------------
+
+# Imports 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+
+# ---------- Create Model Class -------------------
+
+class HannayModel(object):
+    """Driver of ForwardModel simulation"""
+
+
+# Initialize the class
+    def __init__(self):
+        self.set_params() # setting parameters every time an object of the class is created
+
+
+# Set parameter values 
+    def set_params(self):
+        
+        ## Hannay Model
+        self.D = 0
+        self.gamma = 0.024
+        self.K = 0.06358 
+        self.beta = -0.09318
+        self.omega_0 = 0.263524
+
+        self.A_1 = 0.3855 
+        self.beta_L1 = -0.0026
+        self.A_2 = 0.1977 
+        self.beta_L2 = -0.957756
+        self.sigma = 0.0400692
+
+        self.delta = 0.0075
+        self.alpha_0 = 0.05
+        self.p = 1.5
+        self.I_0 = 9325
+        self.G = 33.75
+        
+        
+# Set the light schedule (timings and intensities)
+    def light(self,t,schedule):
+        
+        if schedule == 1: # standard 16:8 schedule 
+            full_light = 1000
+            dim_light = 300
+            wake_time = 7
+            sleep_time = 23
+            sun_up = 8
+            sun_down = 19
+            
+            is_awake = np.mod(t - wake_time,24) <= np.mod(sleep_time - wake_time,24)
+            sun_is_up = np.mod(t - sun_up,24) <= np.mod(sun_down - sun_up,24)
+
+            return is_awake*(full_light*sun_is_up + dim_light*(1 - sun_is_up))
+        else: 
+            return 0
+
+
+# Define the alpha(L) function 
+    def alpha0(self,t,schedule):
+        """A helper function for modeling the light input processing"""
+        
+        return(self.alpha_0*pow(self.light(t,schedule), self.p)/(pow(self.light(t,schedule), self.p)+self.I_0));
+
+        
+
+# Defining the system of ODEs (3-dimensional system)
+    def ODESystem(self,t,y,melatonin_timing,melatonin_dosage,schedule):
+        """
+        This defines the ode system for the single population model and 
+        returns dydt numpy array.
+        """
+        
+        R = y[0]
+        Psi = y[1]
+        n = y[2]
+
+        
+        # Pineal activation/deactivation 
+        psi = Psi #t*(np.pi/12) + (8*np.pi/12) # Convert hours to radians, shift to align with Hannay's convention 
+        if (np.mod(psi,2*np.pi) > self.psi_on) and (np.mod(psi,2*np.pi) < self.psi_off): 
+            A = self.a*((1 - np.exp(-self.delta_M*np.mod(psi - self.psi_on,2*np.pi))/1 - np.exp(-self.delta_M*np.mod(self.psi_off - self.psi_on,2*np.pi))));
+        else: 
+            A = self.a*(np.exp(-self.r*np.mod(psi - self.psi_off,2*np.pi)))
+    
+    
+        # Light interaction with pacemaker
+        Bhat = self.G*(1.0-n)*self.alpha0(t,schedule)
+        #print(Bhat)
+        # Light forcing equations
+        LightAmp = (self.A_1/2.0)*Bhat*(1.0 - pow(R,4.0))*np.cos(Psi + self.beta_L1) + (self.A_2/2.0)*Bhat*R*(1.0 - pow(R,8.0))*np.cos(2.0*Psi + self.beta_L2) # L_R
+        LightPhase = self.sigma*Bhat - (self.A_1/2.0)*Bhat*(pow(R,3.0) + 1.0/R)*np.sin(Psi + self.beta_L1) - (self.A_2/2.0)*Bhat*(1.0 + pow(R,8.0))*np.sin(2.0*Psi + self.beta_L2) # L_psi
+    
+        # Melatonin interaction with pacemaker
+        Mhat = self.M_max/(1 + np.exp((self.H_sat - H2)/self.sigma_M))
+        #print(Mhat)
+        # Melatonin forcing equations 
+        MelAmp = (self.B_1/2)*Mhat*(1.0 - pow(R,4.0))*np.cos(Psi + self.theta_M1) + (self.B_2/2.0)*Mhat*R*(1.0 - pow(R,8.0))*np.cos(2.0*Psi + self.theta_M2) # M_R
+        MelPhase = self.epsilon*Mhat - (self.B_1/2.0)*Mhat*(pow(R,3.0)+1.0/R)*np.sin(Psi + self.theta_M1) - (self.B_2/2.0)*Mhat*(1.0 + pow(R,8.0))*np.sin(2.0*Psi + self.theta_M2) # M_psi
+    
+        # Switch pineal on and off in the presence of light 
+        tmp = 1 - self.m*Bhat
+        S = np.piecewise(tmp, [tmp >= 0, tmp < 0 and H1 < 0.001], [1, 0])
+    
+    
+        dydt=np.zeros(6)
+    
+        # ODE System  
+        dydt[0] = -(self.D + self.gamma)*R + (self.K/2)*np.cos(self.beta)*R*(1 - pow(R,4.0)) + LightAmp + MelAmp # dR/dt
+        dydt[1] = self.omega_0 + (self.K/2)*np.sin(self.beta)*(1 + pow(R,4.0)) + LightPhase + MelPhase # dpsi/dt 
+        dydt[2] = 60.0*(self.alpha0(t,schedule)*(1.0-n)-(self.delta*n)) # dn/dt
+        
+        dydt[3] = -self.beta_IP*H1 + A*(1 - self.m*Bhat)*S # dH1/dt
+        dydt[4] = self.beta_IP*H1 - self.beta_CP*H2 + self.beta_AP*H3 # dH2/dt
+        dydt[5] = -self.beta_AP*H3 + self.ex_melatonin(t,melatonin_timing,melatonin_dosage) # dH3/dt
+
+        return(dydt)
+
+
+
+    def integrateModel(self, tend, tstart=0.0, initial=[1.0, 0.0, 0.0, 0.0, 0.0, 0.0], melatonin_timing = None, melatonin_dosage=None, schedule = 1):
+        """ 
+        Integrate the model forward in time.
+            tend: float giving the final time to integrate to
+            initial=[H1, H2]: initial dynamical state (initial 
+            conditions)
+        Writes the integration results into the scipy array self.results.
+        Returns the circadian phase (in hours) at the ending time for the system.
+        """
+
+        dt = 0.1
+        self.ts = np.arange(tstart,tend,dt)
+        initial[1] = np.mod(initial[1], 2*sp.pi) #start the initial phase between 0 and 2pi
+    
+        # Sanitize input: sometimes np.arange witll output values outside of the range of tstart -> tend
+        self.ts = self.ts[self.ts <= tend]
+        self.ts = self.ts[self.ts >= tstart]
+        
+        r_variable = sp.integrate.solve_ivp(self.ODESystem,(tstart,tend), initial, t_eval=self.ts, method='Radau', args=(melatonin_timing,melatonin_dosage,schedule,))
+        self.results = np.transpose(r_variable.y)
+
+        return
+#-------- end of HannayBreslowModel class ---------
+
+
+
+
+#------------- Running the model under all condtions for the PRC 
+# Set the DLMO threshold that will be used throughout to determine DLMO time
+DLMO_threshold = 10
+
+
+
+
+#--------- Run the model to find initial conditions ---------------
+
+model_IC = HannayBreslowModel() # defining model as a new object built with the HannayBreslowModel class 
+model_IC.integrateModel(24*50,schedule=1) # use the integrateModel method with the object model
+IC = model_IC.results[-1,:] # get initial conditions from entrained model
+
+
+
+#--------- Run the model without exogenous melatonin ---------------
+
+model = HannayBreslowModel()
+#model.integrateModel(24*1,tstart=0.0,initial=IC, melatonin_timing=None, melatonin_dosage=None,schedule=2) 
+
+
+
+
+#--------- Run the model with exogenous melatonin ---------------
+# Set melatonin_timing to a clock hour 
+# Set melatonin dosage to a mg amount
+ 
+#model = HannayBreslowModel()
+model.integrateModel(24*2,tstart=0.0,initial=IC, melatonin_timing=15, melatonin_dosage=0.3,schedule=2) 
+
+
+
+
+
+
+
+
+#--------- Plot Model Output -------------------
+
+'''
+# Plotting H1, H2, and H3 (melatonin concentrations, pmol/L)
+plt.plot(model.ts,model.results[:,3],lw=2)
+plt.plot(model.ts,model.results[:,4],lw=2)
+plt.plot(model.ts,model.results[:,5],lw=2)
+plt.axvline(x=6)
+plt.axvline(x=20.3)
+#plt.axvline(x=12)
+plt.axhline(200)
+plt.xlabel("Time (hours)")
+plt.ylabel("Melatonin Concentration (pmol/L)")
+plt.title("Melatonin Concentrations (pmol/L)")
+plt.legend(["Pineal","Plasma", "Exogenous"])
+plt.show()
+'''
+
+
+# Plotting H1, H2, and H3 (melatonin concentrations, pg/mL)
+plt.plot(model.ts,model.results[:,3]/4.3,lw=2)
+plt.plot(model.ts,model.results[:,4]/4.3,lw=2)
+plt.plot(model.ts,model.results[:,5]/4.3,lw=2)
+#plt.axvline(x=21.4)
+#plt.axvline(x=7)
+#plt.axhline(70)
+plt.axhline(10, linestyle='dashed')
+plt.xlabel("Time (hours)")
+plt.ylabel("Melatonin Concentration (pg/mL)")
+plt.title("Melatonin Concentrations (pg/mL)")
+plt.legend(["Pineal","Plasma", "Exogenous"])
+plt.show()
+
+
+
+# Plotting R
+plt.plot(model.ts,model.results[:,0],lw=2)
+plt.xlabel("Time (hours)")
+plt.ylabel("R, Collective Amplitude")
+plt.title("Time Trace of R, Collective Amplitude")
+plt.show()
+
+
+# Plotting psi
+plt.plot(model.ts,model.results[:,1],lw=2)
+plt.xlabel("Time (hours)")
+plt.ylabel("Psi, Mean Phase (radians)")
+plt.title("Time Trace of Psi, Mean Phase")
+plt.show()
+
+# Plotting psi mod 2pi
+plt.plot(model.ts,np.mod(model.results[:,1],2*np.pi),'o')#lw=2)
+plt.axhline(1.04719755)
+plt.axhline(3.92699)
+plt.axvline(x=7)
+plt.axvline(x=23)
+plt.xlabel("Time (hours)")
+plt.ylabel("Psi, Mean Phase (radians)")
+plt.title("Time Trace of Psi, Mean Phase")
+plt.show()
+
+# Plotting n
+plt.plot(model.ts,model.results[:,2],lw=2)
+#plt.axvline(x=7)
+#plt.axvline(x=23)
+plt.xlabel("Time (hours)")
+plt.ylabel("Proportion of Activated Photoreceptors")
+plt.title("Time Trace of Photoreceptor Activation")
+plt.show()
