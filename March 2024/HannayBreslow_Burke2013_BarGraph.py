ab2958592eafb21e2bebe22fa3b70ee4bf1aced5
Shelby Stowe (Student) <sstowe@mines.edu>
2024-03-28 15:43:01 -0600

    Adding error bars

--- March 2024/HannayBreslow_Burke2013_BarGraph.py
+++ March 2024/HannayBreslow_Burke2013_BarGraph.py
@@ -39,18 +39,30 @@ plt.rc('font', **font)
    
 model = [-0.2, 0.2, 0.8, 1.3] 
 Burke2013 = [-0.43, 0.19, 0.27, 0.7] 
+
+# SD
+error_SD = [0.16, 0.16, 0.15, 0.19]
+
+# Interindividual Variability 
+II_max = [0.44, 0.54, 0.63, 0.81]
+II_min = [0.63, 1.21, 1.08, 0.79]
+
   
 n=4
 r = np.arange(n) 
 width = 0.25
   
-  
-plt.bar(r, model, color = 'mediumturquoise', 
+
+plt.bar(r, Burke2013, color = 'grey', 
+        width = width, edgecolor = 'black', 
+        label='Burke 2013 Data')   
+plt.errorbar(r, Burke2013, yerr=error_SD,color='black',fmt='none',capsize=6,lw=3)
+eb2 = plt.errorbar(r, Burke2013, yerr=[II_min, II_max],fmt='none',color='black',capsize=5)
+eb2[-1][0].set_linestyle('-.')
+plt.bar(r + width, model, color = 'mediumturquoise', 
         width = width, edgecolor = 'black', 
         label='Model') 
-plt.bar(r + width, Burke2013, color = 'grey', 
-        width = width, edgecolor = 'black', 
-        label='Data') 
+
   
 plt.xlabel("Experimental Condition") 
 plt.ylabel("Phase Shift (h)") 
