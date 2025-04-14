adb596328ab6bd08401c7dcbbfa57bf3211cfb03
Shelby Stowe <sstowe@mines.edu>
2025-04-14 16:21:11 -0600

    Changing plot color

--- Preliminary Work - Interindividual Differences/HannayBreslow_Burke2013_BarGraph_II-Diff.py
+++ Preliminary Work - Interindividual Differences/HannayBreslow_Burke2013_BarGraph_II-Diff.py
@@ -61,7 +61,7 @@ plt.bar(r, Burke2013, color = 'grey',
 plt.errorbar(r, Burke2013, yerr=error_SD,color='black',fmt='none',capsize=6,lw=3)
 eb2 = plt.errorbar(r, Burke2013, yerr=[II_min, II_max],fmt='none',color='black',capsize=5)
 eb2[-1][0].set_linestyle('-.')
-plt.bar(r + width, model, color = 'mediumturquoise', 
+plt.bar(r + width, model, color = 'royalblue', 
         width = width, edgecolor = 'black', 
         label='Model') 
 
@@ -71,16 +71,16 @@ plt.ylabel("Phase Shift (h)")
   
 # plt.grid(linestyle='--') 
 plt.xticks(r + width/2,['DLP','DLM','BLP','BLM']) 
-plt.axhline(0,linestyle='dashed',color='black')
-plt.plot(r + width, [-0.21897837755552274, -0.21897837755552274, 0.7634377932709313, 0.7634377932709313],'o',color = 'blue',label = 'Delayed Light History')
-plt.plot(r + width, [-0.23676848429042252, 0.17465555156651646, 0.7786504182689384, 1.2250925127694217],'o',color = 'red',label = 'Advanced Light History')
-plt.plot(r + width, [-0.2294450911619066, 0.16380992318019594, 0.8090005638228916, 1.2395028425216879],'o',color = 'yellow',label = 'Low production')
-plt.plot(r + width, [-0.18316323309520754, 0.1807228813350079, 0.8679435614973343, 1.2585500980756947],'o',color = 'black',label = 'High production')
-plt.plot(r + width, [-0.17115303825973882, -0.17115695697379607, 0.8756813789396247, 0.8765825262056737],'o',color = 'orange',label = 'Early Chronotype')
-plt.plot(r + width, [-0.3234762446624444, -0.3234762446624444, 0.7132851442735699, 0.7132851442735699],'o',color = 'midnightblue',label = 'Late Chronotype')
-plt.plot(r + width, [-0.23653804417646285, 0.18080009999820135, 0.7690794651279518, 1.221421273841564],'o',color = 'fuchsia',label = 'Increased Light Intensity')
-plt.plot(r + width, [-0.2363937376018157, 0.08931684235497528, 0.9157121172801084, 1.2677500901559142],'o',color = 'purple',label = 'Decreased Light Intensity')
+plt.plot(r + width, [-0.21897837755552274, -0.21897837755552274, 0.7634377932709313, 0.7634377932709313],'o',color = 'darkorange', label = 'Delayed Light History')
+plt.plot(r + width, [-0.23676848429042252, 0.17465555156651646, 0.7786504182689384, 1.2250925127694217],'o',color = 'darkorange',label = 'Advanced Light History')
+plt.plot(r + width, [-0.2294450911619066, 0.16380992318019594, 0.8090005638228916, 1.2395028425216879],'o',color = 'darkorange',label = 'Low production')
+plt.plot(r + width, [-0.18316323309520754, 0.1807228813350079, 0.8679435614973343, 1.2585500980756947],'o',color = 'darkorange',label = 'High production')
+plt.plot(r + width, [-0.17115303825973882, -0.17115695697379607, 0.8756813789396247, 0.8765825262056737],'o',color = 'darkorange',label = 'Early Chronotype')
+plt.plot(r + width, [-0.3234762446624444, -0.3234762446624444, 0.7132851442735699, 0.7132851442735699],'o',color = 'darkorange',label = 'Late Chronotype')
+plt.plot(r + width, [-0.23653804417646285, 0.18080009999820135, 0.7690794651279518, 1.221421273841564],'o',color = 'darkorange',label = 'Increased Light Intensity')
+plt.plot(r + width, [-0.2363937376018157, 0.08931684235497528, 0.9157121172801084, 1.2677500901559142],'o',color = 'darkorange',label = 'Decreased Light Intensity')
 #plt.plot(r + width, [-0.29966864361298207, -0.29966864361298207, 0.7390668527878788, 0.7390668527878788],'o',color = 'green',label = 'High Production + Late Chronotype')
-plt.legend(loc ='center right', bbox_to_anchor=(1.7, 0.5)) 
+plt.axhline(0,linestyle='dashed',color='black')
+#plt.legend(["Model", "Data"], loc ='center right', bbox_to_anchor=(1.7, 0.5)) 
 plt.show()
 
